fcead2fe31a4832e6baa51029d18f64f16f364eb
Brane Opacic <brane.opacic@brightmarbles.io>
2018-05-04 09:25:08 +0100

    Added basic wireframe styles for gallery container

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Auxx from '../../hoc/Auxx/Auxx';
 import GalleryItem from './GalleryItem/GalleryItem';
+import classes from './Gallery.css';
 
 const gallery = (props) => {
 
     
     return (
         <Auxx>
-            <GalleryItem />
+            <div className={classes.Gallery}>
+                <div className={classes.GalleryInside}>
+                    <GalleryItem />
+                    2
+                </div>
+            </div>
         </Auxx>
     )
 }
--- src/components/Gallery/GalleryItem/GalleryItem.js
+++ src/components/Gallery/GalleryItem/GalleryItem.js
@@ -3,9 +3,12 @@ import Auxx from '../../../hoc/Auxx/Auxx';
 
 const galleryItem = (props) => {
 
-    let fromLocalStorage = JSON.parse(localStorage.getItem('gallery')).map(el => {
-        return <div>{el.xyCord[0]}</div>
-    })
+    let fromLocalStorage;
+    if(localStorage.getItem('gallery')) {
+         fromLocalStorage = JSON.parse(localStorage.getItem('gallery')).map(el => {
+            return <div>{el.xyCord[0]}</div>
+        })
+    }
 
     return (
         <Auxx>
