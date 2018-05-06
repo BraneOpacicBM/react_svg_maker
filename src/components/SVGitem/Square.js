821a61f04bb89f80f459d5f1dc695090f523e3f9
Brane <braneopacic@gmail.com>
2018-05-06 13:46:58 +0200

    Implemented the logic for square 2/4 quardants, the first and the fourth

--- src/components/SVGitem/Square.js
+++ src/components/SVGitem/Square.js
@@ -22,23 +22,46 @@ class Rechtangle extends Component {
         
 
         if(this.props.xyCord.length === 2) {
-            svg = (
-                <svg className={classes.SVG} 
-                height="100%"
-                width="100%"
-                viewBox="0, 0, 100, 100"
-                // viewBox={"0 0 " + this.props.resizedCoord[0] + " " + this.props.resizedCoord[1]}
-                >   
-                <rect 
-                x={this.props.xyCord[0][0] / devider} 
-                y={this.props.xyCord[0][1] / devider}
-                height={Math.abs(this.props.xyCord[1][1] - this.props.xyCord[0][1]) / devider} 
-                width={Math.abs(this.props.xyCord[1][0] - this.props.xyCord[0][0]) / devider} 
-                stroke={this.getRGBA(this.props.rgbaStroke.r, this.props.rgbaStroke.g, this.props.rgbaStroke.b, this.props.rgbaStroke.a )} 
-                fill={this.getRGBA(this.props.rgbaFill.r, this.props.rgbaFill.g, this.props.rgbaFill.b, this.props.rgbaFill.a)} />
-                Sorry, your browser does not support inline SVG.  
-                </svg>
-            )
+            if(this.props.xyCord[0][0] > this.props.xyCord[1][0] && this.props.xyCord[0][1] > this.props.xyCord[1][1] ) {
+                 svg = (
+                    <svg className={classes.SVG} 
+                    height="100%"
+                    width="100%"
+                    viewBox="0, 0, 100, 100"
+                    // viewBox={"0 0 " + this.props.resizedCoord[0] + " " + this.props.resizedCoord[1]}
+                    >   
+                    <rect 
+                    x={this.props.xyCord[1][0] / devider} 
+                    y={this.props.xyCord[1][1] / devider}
+                    height={Math.abs(this.props.xyCord[0][1] - this.props.xyCord[1][1]) / devider} 
+                    width={Math.abs(this.props.xyCord[0][1] - this.props.xyCord[1][1]) / devider}  
+                    stroke={this.getRGBA(this.props.rgbaStroke.r, this.props.rgbaStroke.g, this.props.rgbaStroke.b, this.props.rgbaStroke.a )} 
+                    fill={this.getRGBA(this.props.rgbaFill.r, this.props.rgbaFill.g, this.props.rgbaFill.b, this.props.rgbaFill.a)} />
+                    Sorry, your browser does not support inline SVG.  
+                    </svg>
+                )
+
+            }
+
+            if(this.props.xyCord[0][0] < this.props.xyCord[1][0] && this.props.xyCord[0][1] < this.props.xyCord[1][1] ) {
+                 svg = (
+                    <svg className={classes.SVG} 
+                    height="100%"
+                    width="100%"
+                    viewBox="0, 0, 100, 100"
+                    // viewBox={"0 0 " + this.props.resizedCoord[0] + " " + this.props.resizedCoord[1]}
+                    >   
+                    <rect 
+                    x={this.props.xyCord[0][0] / devider} 
+                    y={this.props.xyCord[0][1] / devider}
+                    height={Math.abs(this.props.xyCord[1][1] - this.props.xyCord[0][1]) / devider} 
+                    width={Math.abs(this.props.xyCord[1][1] - this.props.xyCord[0][1]) / devider}  
+                    stroke={this.getRGBA(this.props.rgbaStroke.r, this.props.rgbaStroke.g, this.props.rgbaStroke.b, this.props.rgbaStroke.a )} 
+                    fill={this.getRGBA(this.props.rgbaFill.r, this.props.rgbaFill.g, this.props.rgbaFill.b, this.props.rgbaFill.a)} />
+                    Sorry, your browser does not support inline SVG.  
+                    </svg>
+                )
+            }
         }
 
         return svg;
